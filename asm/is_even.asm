2d62ad6d4d6667326a1e9523dd4135366e530077
sthaarwin <sthaarwin123@gmail.com>
2024-09-07 17:01:07 +0545

    fiex floating point exception

--- asm/is_even.asm
+++ asm/is_even.asm
@@ -8,16 +8,14 @@ section .data
     is_odd db "odd." , 0
     is_odd_len equ $-is_odd
 
-
 section .bss
     num resb 10
-    a resd 1
 
 section .text
     extern atoi
     global _start
 
-_start:    
+_start:
     mov eax, 4
     mov ebx, 1
     mov ecx, ask
@@ -30,30 +28,22 @@ _start:
     mov edx, 10
     int 0x80
 
-    add byte[num + eax - 1], 0 
+    add byte[num + eax - 1], 0
     push num
     call atoi
     add esp, 4
 
-    mov [num], eax
-
-    mov eax, [num]
+    mov ebx, eax
     mov edx, 0
+
+    mov eax, ebx
     mov ebx, 2
-    div eax
+    div ebx
 
-    cmp edx, 0
-    je even
 
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, is_odd
-    mov edx, is_odd_len
-    int 0x80
-
-    jmp exit
-
-even : 
+    cmp edx, 1
+    je odd
+    
     mov eax, 4
     mov ebx, 1
     mov ecx, is_even
@@ -62,9 +52,16 @@ even :
 
     jmp exit
 
+odd:
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, is_odd
+    mov edx, is_odd_len
+    int 0x80
+
+    jmp exit
+
 exit:
     mov eax, 1
     mov ebx, 0
     int 0x80
-
-    
